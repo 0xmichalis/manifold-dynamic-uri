cda2ffacad41df8482ed2c4a07823bb50bc1e6df
Michalis Kargakis <kargakis@protonmail.com>
2024-02-11 19:59:43 +0100

    Add a couple of comments and format code

--- src/DynamicTokenURI.sol
+++ src/DynamicTokenURI.sol
@@ -6,13 +6,8 @@ import {ICreatorExtensionTokenURI} from "manifoldxyz/creator-core/extensions/ICr
 import {IERC721} from "openzeppelin/token/ERC721/IERC721.sol";
 import {IERC165} from "openzeppelin/utils/introspection/ERC165.sol";
 
-
 contract DynamicTokenURI is ICreatorExtensionTokenURI {
-    function supportsInterface(bytes4 interfaceId)
-        public
-        pure
-        returns (bool)
-    {
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
         return interfaceId == type(ICreatorExtensionTokenURI).interfaceId;
     }
 
@@ -24,10 +19,20 @@ contract DynamicTokenURI is ICreatorExtensionTokenURI {
         uint256 addressNumber = uint256(uint160(tokenOwner));
         // TODO: This should be read from the creator contract
         uint256 totalNumberOfArtworks = 69;
-        uint256 artworkId = (addressNumber % totalNumberOfArtworks) + 1;
+        // Naive way to rotate artworks for a token while the token
+        // is transferred between owners.
+        uint256 metadataId = (addressNumber % totalNumberOfArtworks) + 1;
 
+        // This assumes the following directory structure in IPFS:
+        // .
+        // └──<ipfs_directory>
+        //    ├── 1.json
+        //    ├── 2.json
+        //    ├── ...
+        //    └── <totalNumberOfArtworks>.json
+        //
         // TODO: Read the IPFS directory from the creator contract
         // or allow setting it in this contract
-        return string(abi.encodePacked("ipfs://<ipfs_directory>/", artworkId, ".json"));
+        return string(abi.encodePacked("ipfs://<ipfs_directory>/", metadataId, ".json"));
     }
 }
