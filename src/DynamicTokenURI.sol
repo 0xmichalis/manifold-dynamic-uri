9f8746e56ded55e74a9e0e34e08d4e3cd82214f8
Michalis Kargakis <kargakis@protonmail.com>
2024-02-13 10:31:34 +0100

    Extension is ownable and can approve transfers

--- src/DynamicTokenURI.sol
+++ src/DynamicTokenURI.sol
@@ -2,13 +2,21 @@
 
 pragma solidity ^0.8.17;
 
+import {IERC721CreatorCore} from "manifoldxyz/creator-core/core/IERC721CreatorCore.sol";
 import {ICreatorExtensionTokenURI} from "manifoldxyz/creator-core/extensions/ICreatorExtensionTokenURI.sol";
+import {IERC721CreatorExtensionApproveTransfer} from
+    "manifoldxyz/creator-core/extensions/ERC721/IERC721CreatorExtensionApproveTransfer.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {IERC721} from "openzeppelin/token/ERC721/IERC721.sol";
 import {IERC165} from "openzeppelin/utils/introspection/ERC165.sol";
+import {ERC165Checker} from "openzeppelin/utils/introspection/ERC165Checker.sol";
 
-contract DynamicTokenURI is ICreatorExtensionTokenURI {
+contract DynamicTokenURI is Ownable, ICreatorExtensionTokenURI, IERC721CreatorExtensionApproveTransfer {
+    constructor() Ownable(msg.sender) {}
+
     function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
-        return interfaceId == type(ICreatorExtensionTokenURI).interfaceId;
+        return interfaceId == type(Ownable).interfaceId || interfaceId == type(ICreatorExtensionTokenURI).interfaceId
+            || interfaceId == type(IERC721CreatorExtensionApproveTransfer).interfaceId;
     }
 
     function tokenURI(address creator, uint256 tokenId) external view override returns (string memory) {
@@ -34,5 +42,23 @@ contract DynamicTokenURI is ICreatorExtensionTokenURI {
         // TODO: Read the IPFS directory from the creator contract
         // or allow setting it in this contract
         return string(abi.encodePacked("ipfs://<ipfs_directory>/", metadataId, ".json"));
+    }
+
+    /**
+     * @dev Set whether or not the creator will check the extension for approval of token transfer
+     */
+    function setApproveTransfer(address creator, bool enabled) external {
+        require(
+            ERC165Checker.supportsInterface(creator, type(IERC721CreatorCore).interfaceId),
+            "creator must implement IERC721CreatorCore"
+        );
+        IERC721CreatorCore(creator).setApproveTransferExtension(enabled);
+    }
+
+    /**
+     * @dev Called by creator contract to approve a transfer
+     */
+    function approveTransfer(address operator, address from, address to, uint256 tokenId) external returns (bool) {
+        return true;
     }
 }
